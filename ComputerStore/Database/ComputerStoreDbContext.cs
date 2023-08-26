a2a1207ee8fe7e3d3d5cf7dfed5de6f05b9baed9
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-26 00:39:01 -0700

    Mapping ids.

--- ComputerStore/Database/ComputerStoreDbContext.cs
+++ ComputerStore/Database/ComputerStoreDbContext.cs
@@ -12,6 +12,13 @@ namespace ComputerStore.Database
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.MapIds();
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Category> Categories { get; set; }
