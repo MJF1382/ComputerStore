0b5c862b67013d3ba81ebeb9581a34f7f4638d32
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-26 01:47:13 -0700

    Creating method to map product in dbcontext.

--- ComputerStore/Database/Mapping.cs
+++ ComputerStore/Database/Mapping.cs
@@ -1,5 +1,6 @@
 ﻿using ComputerStore.Database.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ComputerStore.Database
 {
@@ -7,16 +8,31 @@ namespace ComputerStore.Database
     {
         public static void MapIds(this ModelBuilder modelBuilder)
         {
+            var d = modelBuilder.Entity<Product>();
+
             modelBuilder.Entity<Product>().HasKey(p => p.Id);
             modelBuilder.Entity<Article>().HasKey(p => p.Id);
             modelBuilder.Entity<Category>().HasKey(p => p.Id);
-            modelBuilder.Entity<Brands>().HasKey(p => p.Id);
+            modelBuilder.Entity<Brand>().HasKey(p => p.Id);
             modelBuilder.Entity<ExtraDetail>().HasKey(p => p.Id);
             modelBuilder.Entity<Ticket>().HasKey(p => p.Id);
             modelBuilder.Entity<Comment>().HasKey(p => p.Id);
             modelBuilder.Entity<Purchase>().HasKey(p => p.Id);
             modelBuilder.Entity<ProductPurchase>().HasKey(p => new { p.ProductId, p.PurchaseId });
             modelBuilder.Entity<ArticleTag>().HasKey(p => new { p.ArticleId, p.TagId });
+        }
+
+        public static void MapProduct(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Brand)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.BrandId);
         }
     }
 }
