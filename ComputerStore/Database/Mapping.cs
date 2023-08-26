761bb8ac83f16f169e799edc76f11fc6b0151137
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-26 00:38:54 -0700

    Creating method to map ids in dbcontext.

---
+++ ComputerStore/Database/Mapping.cs
@@ -0,0 +1,22 @@
+﻿using ComputerStore.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStore.Database
+{
+    public static class Mapping
+    {
+        public static void MapIds(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Article>().HasKey(p => p.Id);
+            modelBuilder.Entity<Category>().HasKey(p => p.Id);
+            modelBuilder.Entity<Brands>().HasKey(p => p.Id);
+            modelBuilder.Entity<ExtraDetail>().HasKey(p => p.Id);
+            modelBuilder.Entity<Ticket>().HasKey(p => p.Id);
+            modelBuilder.Entity<Comment>().HasKey(p => p.Id);
+            modelBuilder.Entity<Purchase>().HasKey(p => p.Id);
+            modelBuilder.Entity<ProductPurchase>().HasKey(p => new { p.ProductId, p.PurchaseId });
+            modelBuilder.Entity<ArticleTag>().HasKey(p => new { p.ArticleId, p.TagId });
+        }
+    }
+}
