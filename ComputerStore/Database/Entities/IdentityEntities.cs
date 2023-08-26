e343320e1c556e68539a80ee28fc94f6e8994258
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-26 09:58:09 -0700

    Adding "AppUser" entity.

---
+++ ComputerStore/Database/Entities/IdentityEntities.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Database.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        public DateTime BirthDay { get; set; }
+    }
+}
