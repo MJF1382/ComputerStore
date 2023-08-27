aca6072c8edc12ebfb6a44093622a41e9168fbd5
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 01:08:52 -0700

    Regenerate database.

---
+++ ComputerStore/Migrations/20230827080734_RegenerateDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class RegenerateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDay",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AppUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDay",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AppUsers");
+        }
+    }
+}
