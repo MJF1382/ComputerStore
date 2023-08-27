5c7fd9d8831e48c7b84141ff72d074031d732e47
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 09:59:25 -0700

    Adding "IRepositoryBase" interface.

---
+++ ComputerStore/Database/Repositories/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace ComputerStore.Database.Repositories
+{
+    public interface IRepositoryBase<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> FindByIdAsync(object id);
+        Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> condition);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object id);
+    }
+}
