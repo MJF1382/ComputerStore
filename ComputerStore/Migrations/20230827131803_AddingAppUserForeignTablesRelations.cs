8ecdd38d7dadde5d6506beb70cf08caf96f4f42e
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 06:28:41 -0700

    Adding "AddingAppUserForeignTablesRelations" migration.

---
+++ ComputerStore/Migrations/20230827131803_AddingAppUserForeignTablesRelations.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class AddingAppUserForeignTablesRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Tickets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_UserId",
+                table: "Purchases",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_UserId",
+                table: "Articles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AppUsers_UserId",
+                table: "Articles",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AppUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_AppUsers_UserId",
+                table: "Purchases",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_AppUsers_UserId",
+                table: "Tickets",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AppUsers_UserId",
+                table: "Articles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AppUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_AppUsers_UserId",
+                table: "Purchases");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_AppUsers_UserId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_UserId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_UserId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tickets");
+        }
+    }
+}
