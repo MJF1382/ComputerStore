a7b2563474413389d218ece931285c21ee33836c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 12:21:49 -0700

    Adding "IUnitOfWork" interface.

---
+++ ComputerStore/Database/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using ComputerStore.Database.Repositories;
+
+namespace ComputerStore.Database.UnitOfWork
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IRepositoryBase<TEntity> RepositoryBase<TEntity>() where TEntity : class;
+        Task<bool> Save();
+    }
+}
