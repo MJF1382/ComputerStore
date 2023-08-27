d792c449cb5b32dc7333cd64c8d833d8143b688a
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 10:00:03 -0700

    Implementing repositorybase based on "IRepositoryBase" interface.

---
+++ ComputerStore/Database/Repositories/RepositoryBase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ComputerStore.Database.Repositories
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> FindByIdAsync(object id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> condition)
+        {
+            return await _context.Set<TEntity>().Where(condition).ToListAsync();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddRangeAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public void Delete(object id)
+        {
+            TEntity? entity = FindByIdAsync(id).Result;
+
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Remove(entity);
+            }
+        }
+    }
+}
