1da2c8e328fc314c23650ad21a6ed5211f8ee666
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-27 12:22:37 -0700

    Implementing UnitOfWork pattern based on "IUnitOfWork" interface.

---
+++ ComputerStore/Database/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using ComputerStore.Database.Repositories;
+
+namespace ComputerStore.Database.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ComputerStoreDbContext _context;
+
+        public UnitOfWork(ComputerStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepositoryBase<TEntity> RepositoryBase<TEntity>() where TEntity : class
+        {
+            return new RepositoryBase<TEntity>(_context);
+        }
+
+        public async Task<bool> Save()
+        {
+            return Convert.ToBoolean(await _context.SaveChangesAsync());
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return _context.DisposeAsync();
+        }
+    }
+}
