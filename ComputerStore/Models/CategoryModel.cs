64158a75fe751c5735e37b60d51a07267b890257
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-28 01:36:16 -0700

    Adding "CategoryModel".

---
+++ ComputerStore/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Models
+{
+    public class CategoryModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
