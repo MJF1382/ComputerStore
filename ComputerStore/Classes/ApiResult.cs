ef0e3f44f09e72c424fab1ca0a695abc122a02b9
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-28 01:35:48 -0700

    Adding "ApiResult" class to send api response.

---
+++ ComputerStore/Classes/ApiResult.cs
@@ -0,0 +1,36 @@
+﻿namespace ComputerStore.Classes
+{
+    public class ApiResult
+    {
+        public int StatusCode { get; private set; }
+        public object? Content { get; private set; }
+        public List<string> Errors { get; private set; }
+
+        public ApiResult(
+            Status statusCode)
+        {
+            StatusCode = (int)statusCode;
+            Content = null;
+            Errors = new List<string>();
+        }
+
+        public ApiResult(
+            Status statusCode,
+            object? content)
+        {
+            StatusCode = (int)statusCode;
+            Content = content;
+            Errors = new List<string>();
+        }
+
+        public ApiResult(
+            Status statusCode,
+            object? content,
+            List<string> errors)
+        {
+            StatusCode = (int)statusCode;
+            Content = content;
+            Errors = errors;
+        }
+    }
+}
