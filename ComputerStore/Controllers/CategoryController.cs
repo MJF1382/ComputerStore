369d155a5e1afc27d4d5b7067d6e2f2ac74866dd
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 01:38:55 -0700

    Adding delete category action.

--- ComputerStore/Controllers/CategoryController.cs
+++ ComputerStore/Controllers/CategoryController.cs
@@ -72,5 +72,19 @@ namespace ComputerStore.Controllers
 
             return new ApiResult(Status.InternalServerError);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ApiResult> DeleteCategory([FromRoute] Guid id)
+        {
+            _categoryRepository.Delete(id);
+            bool result = await _unitOfWork.Save();
+
+            if (result)
+            {
+                return new ApiResult(Status.Ok);
+            }
+
+            return new ApiResult(Status.InternalServerError);
+        }
     }
 }
