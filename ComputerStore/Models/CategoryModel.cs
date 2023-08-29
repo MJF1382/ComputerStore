b98adecf97a2ced4ffd64a78504c0caf2a01721d
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 01:17:02 -0700

    Adding error messages for properties.

--- ComputerStore/Models/CategoryModel.cs
+++ ComputerStore/Models/CategoryModel.cs
@@ -6,8 +6,8 @@ namespace ComputerStore.Models
     {
         public Guid Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "نام دسته بندی را وارد کنید.")]
+        [StringLength(100, ErrorMessage = "نام دسته بندی باید حداکثر 100 کاراکتر باشد.")]
         public string Name { get; set; }
     }
 }
