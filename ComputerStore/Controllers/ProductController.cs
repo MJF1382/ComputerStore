d03d8e2885484ffcf6ac32b90d22e72d1364365b
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 03:04:31 -0700

    Adding get products api.

--- ComputerStore/Controllers/ProductController.cs
+++ ComputerStore/Controllers/ProductController.cs
@@ -1,12 +1,14 @@
-﻿using ComputerStore.Database.Entities;
+﻿using ComputerStore.Classes;
+using ComputerStore.Database.Entities;
 using ComputerStore.Database.Repositories;
 using ComputerStore.Database.UnitOfWork;
+using ComputerStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComputerStore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/products")]
     [ApiController]
     public class ProductController : ControllerBase
     {
@@ -17,6 +19,25 @@ namespace ComputerStore.Controllers
         {
             _unitOfWork = unitOfWork;
             _productRepository = _unitOfWork.RepositoryBase<Product>();
+        }
+
+        [HttpGet]
+        public async Task<ApiResult> GetProducts()
+        {
+            List<ProductModel> products = (await _productRepository.GetAllAsync()).Select(product => new ProductModel()
+            {
+                Id = product.Id,
+                CategoryId = product.CategoryId,
+                BrandId = product.BrandId,
+                Title = product.Title,
+                Summary = product.Summary,
+                Warranty = product.Warranty,
+                Price = product.Price,
+                OfferedPrice = product.OfferedPrice,
+                Quantity = product.Quantity
+            }).ToList();
+
+            return new ApiResult(Status.Ok, products);
         }
     }
 }
