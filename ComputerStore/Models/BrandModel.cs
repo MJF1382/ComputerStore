81fd5f2ef49bb39f57b3727ebdf477f36a6e2adf
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 06:12:48 -0700

    Adding implicit casting for Brand to BrandModel and vice versa.

--- ComputerStore/Models/BrandModel.cs
+++ ComputerStore/Models/BrandModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ComputerStore.Database.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace ComputerStore.Models
 {
@@ -9,5 +10,23 @@ namespace ComputerStore.Models
         [Required(ErrorMessage = "نام برند را وارد کنید.")]
         [StringLength(100, ErrorMessage = "نام برند باید حداکثر 100 کاراکتر باشد.")]
         public string Name { get; set; }
+
+        public static implicit operator BrandModel(Brand brand)
+        {
+            return new BrandModel()
+            {
+                Id = brand.Id,
+                Name = brand.Name
+            };
+        }
+
+        public static implicit operator Brand(BrandModel brandModel)
+        {
+            return new Brand()
+            {
+                Id = brandModel.Id,
+                Name = brandModel.Name
+            };
+        }
     }
 }
