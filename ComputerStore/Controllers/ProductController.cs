595e76403fa48a311ed1e5ac5175a89e75dfe42f
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 02:00:44 -0700

    Adding product controller.

---
+++ ComputerStore/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using ComputerStore.Database.Entities;
+using ComputerStore.Database.Repositories;
+using ComputerStore.Database.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepositoryBase<Product> _productRepository;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _productRepository = _unitOfWork.RepositoryBase<Product>();
+        }
+    }
+}
