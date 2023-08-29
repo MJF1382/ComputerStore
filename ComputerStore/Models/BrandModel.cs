91b3c75f5570558555703eacdd3b759dd9c2e58f
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-29 01:57:18 -0700

    Adding "BrandModel" class.

---
+++ ComputerStore/Models/BrandModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Models
+{
+    public class BrandModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "نام برند را وارد کنید.")]
+        [StringLength(100, ErrorMessage = "نام برند باید حداکثر 100 کاراکتر باشد.")]
+        public string Name { get; set; }
+    }
+}
