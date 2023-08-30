f0fd24917fb0f98dcf2c0f7e229912e2f096ca15
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-30 09:15:58 -0700

    Adding user controller.

---
+++ ComputerStore/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ComputerStore.Classes;
+using ComputerStore.Database.Entities;
+using ComputerStore.Database.IdentityStores;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStore.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ComputerStoreUserStore _userStore;
+
+        public UserController(ComputerStoreUserStore userStore)
+        {
+            _userStore = userStore;
+        }
+    }
+}
