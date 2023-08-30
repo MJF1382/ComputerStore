a89da2b5be2b0c1b5ca7d659f2ae44f72e1ca443
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-30 09:15:11 -0700

    Customizing user store.

---
+++ ComputerStore/Database/IdentityStores/ComputerStoreUserStore.cs
@@ -0,0 +1,18 @@
+﻿using ComputerStore.Database.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStore.Database.IdentityStores
+{
+    public class ComputerStoreUserStore : UserStore<AppUser, AppRole, ComputerStoreDbContext, Guid, AppUserClaim, AppUserRole, AppUserLogin, AppUserToken, AppRoleClaim>, IComputerStoreUserStore
+    {
+        public ComputerStoreUserStore(
+            ComputerStoreDbContext context,
+            IdentityErrorDescriber describer = null)
+            : base(context, describer)
+        {
+
+        }
+    }
+}
---
+++ ComputerStore/Database/IdentityStores/IComputerStoreUserStore.cs
@@ -0,0 +1,10 @@
+﻿using ComputerStore.Database.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace ComputerStore.Database.IdentityStores
+{
+    public interface IComputerStoreUserStore : IUserStore<AppUser>
+    {
+
+    }
+}
