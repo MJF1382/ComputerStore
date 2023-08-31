54d977db71703cf7245fe909aebb8f45be6439c0
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-31 11:14:01 -0700

    Adding "RoleModel" class.

---
+++ ComputerStore/Models/RoleModel.cs
@@ -0,0 +1,28 @@
+﻿using ComputerStore.Database.Entities;
+
+namespace ComputerStore.Models
+{
+    public class RoleModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator RoleModel(AppRole role)
+        {
+            return new RoleModel()
+            {
+                Id = role.Id,
+                Name = role.Name
+            };
+        }
+
+        public static implicit operator AppRole(RoleModel roleModel)
+        {
+            return new AppRole()
+            {
+                Id = roleModel.Id,
+                Name = roleModel.Name
+            };
+        }
+    }
+}
