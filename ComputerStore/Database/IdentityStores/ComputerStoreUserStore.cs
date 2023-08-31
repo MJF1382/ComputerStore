a9bff6ff5a3949e7821f6ec4ef5a99577e0a4c45
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-31 13:27:21 -0700

    Adding "GetUsersWithRolesAsync" method.

--- ComputerStore/Database/IdentityStores/ComputerStoreUserStore.cs
+++ ComputerStore/Database/IdentityStores/ComputerStoreUserStore.cs
@@ -1,4 +1,5 @@
 ﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -7,12 +8,22 @@ namespace ComputerStore.Database.IdentityStores
 {
     public class ComputerStoreUserStore : UserStore<AppUser, AppRole, ComputerStoreDbContext, Guid, AppUserClaim, AppUserRole, AppUserLogin, AppUserToken, AppRoleClaim>, IComputerStoreUserStore
     {
-        public ComputerStoreUserStore(
-            ComputerStoreDbContext context,
-            IdentityErrorDescriber describer = null)
-            : base(context, describer)
+        public ComputerStoreUserStore(ComputerStoreDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
         {
+            AutoSaveChanges = false;
+        }
 
+        public async Task<List<UserModel>> GetUsersWithRolesAsync()
+        {
+            var users = await Users.Include(p => p.UserRoles).ThenInclude(p => p.Role).ToListAsync();
+            List<UserModel> userModels = users.Select<AppUser, UserModel>(user => user).ToList();
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                userModels[i].RoleIds = users[i].UserRoles.Select(userRole => userRole.Role.Name).ToList();
+            }
+
+            return userModels;
         }
     }
 }
--- ComputerStore/Database/IdentityStores/IComputerStoreUserStore.cs
+++ ComputerStore/Database/IdentityStores/IComputerStoreUserStore.cs
@@ -1,10 +1,13 @@
 ﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComputerStore.Database.IdentityStores
 {
     public interface IComputerStoreUserStore : IUserStore<AppUser>
     {
-
+        Task<List<UserModel>> GetUsersWithRolesAsync();
     }
 }
