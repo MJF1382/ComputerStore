c639c2753606ba0b4607f5ac15d15ca052fb6ecb
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-31 11:13:35 -0700

    Adding role controller.

---
+++ ComputerStore/Controllers/RoleController.cs
@@ -0,0 +1,30 @@
+﻿using ComputerStore.Classes;
+using ComputerStore.Database.Entities;
+using ComputerStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStore.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<ApiResult> GetRoles()
+        {
+            List<RoleModel> roles = await _roleManager.Roles.Select<AppRole, RoleModel>(role => role).ToListAsync();
+
+            return new ApiResult(Status.Ok, roles);
+        }
+    }
+}
