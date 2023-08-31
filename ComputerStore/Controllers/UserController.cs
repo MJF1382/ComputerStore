7230530c203d153f86729c29a2cbe8bfcd18669b
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-31 13:26:23 -0700

    Injecting "IComputerStoreUserStore" and "UserManager" in "UserController" controller.

--- ComputerStore/Controllers/UserController.cs
+++ ComputerStore/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using ComputerStore.Classes;
 using ComputerStore.Database.Entities;
 using ComputerStore.Database.IdentityStores;
+using ComputerStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,11 +12,16 @@ namespace ComputerStore.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly ComputerStoreUserStore _userStore;
+        private readonly IComputerStoreUserStore _userStore;
+        private readonly UserManager<AppUser> _userManager;
 
-        public UserController(ComputerStoreUserStore userStore)
+        public UserController(
+            IComputerStoreUserStore userStore,
+            UserManager<AppUser> userManager
+            )
         {
             _userStore = userStore;
+            _userManager = userManager;
         }
     }
 }
