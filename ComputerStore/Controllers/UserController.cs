0a625a3a62739d8a5d36b29b566f872e41950261
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 06:24:11 -0700

    Using customized user manager and showing errors.

--- ComputerStore/Controllers/UserController.cs
+++ ComputerStore/Controllers/UserController.cs
@@ -1,6 +1,6 @@
 ﻿using ComputerStore.Classes;
 using ComputerStore.Database.Entities;
-using ComputerStore.Database.IdentityStores;
+using ComputerStore.Database.IdentityProviders;
 using ComputerStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -12,27 +12,23 @@ namespace ComputerStore.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IComputerStoreUserStore _userStore;
-        private readonly UserManager<AppUser> _userManager;
+        private readonly AppUserManager _userManager;
 
-        public UserController(
-            IComputerStoreUserStore userStore,
-            UserManager<AppUser> userManager
-            )
+        public UserController(AppUserManager userManager)
         {
-            _userStore = userStore;
             _userManager = userManager;
         }
 
         [HttpGet]
         public async Task<ApiResult> GetUsers()
         {
-            return new ApiResult(Status.Ok, await _userStore.GetUsersWithRolesAsync());
+            return new ApiResult(Status.Ok, await _userManager.GetUsersWithRolesAsync());
         }
 
         [HttpPost]
         public async Task<ApiResult> AddUser([FromBody] UserModel userModel)
         {
+            List<string> errors = new List<string>();
             userModel.UserName = userModel.Email;
 
             IdentityResult result = await _userManager.CreateAsync(userModel, userModel.Password);
@@ -42,7 +38,6 @@ namespace ComputerStore.Controllers
                 if (userModel.RoleIds.Count > 0)
                 {
                     AppUser user = await _userManager.FindByNameAsync(userModel.UserName);
-
                     result = await _userManager.AddToRolesAsync(user, userModel.RoleIds);
 
                     if (result.Succeeded)
@@ -51,10 +46,32 @@ namespace ComputerStore.Controllers
 
                         return new ApiResult(Status.Created, userModel);
                     }
+                    else
+                    {
+                        await _userManager.DeleteAsync(user);
+
+                        foreach (string errorMessage in result.Errors.Select(identityError => identityError.Description))
+                        {
+                            errors.Add(errorMessage);
+                        }
+                    }
+                }
+                else
+                {
+                    errors.Add("نقش های کاربر را وارد کنید.");
+
+                    return new ApiResult(Status.BadRequest, null, errors);
+                }
+            }
+            else
+            {
+                foreach (string errorMessage in result.Errors.Select(identityError => identityError.Description))
+                {
+                    errors.Add(errorMessage);
                 }
             }
 
-            return new ApiResult(Status.InternalServerError);
+            return new ApiResult(Status.InternalServerError, null, errors);
         }
     }
 }
