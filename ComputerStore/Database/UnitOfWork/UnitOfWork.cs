a290c4a01cfbc3293bf532c42bab43b7e0703e14
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 11:49:55 -0700

    Implementing transaction methods.

--- ComputerStore/Database/UnitOfWork/UnitOfWork.cs
+++ ComputerStore/Database/UnitOfWork/UnitOfWork.cs
@@ -1,10 +1,12 @@
 ﻿using ComputerStore.Database.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ComputerStore.Database.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ComputerStoreDbContext _context;
+        private IDbContextTransaction? _transaction;
 
         public UnitOfWork(ComputerStoreDbContext context)
         {
@@ -13,16 +15,38 @@ namespace ComputerStore.Database.UnitOfWork
 
         public IRepositoryBase<TEntity> RepositoryBase<TEntity>() where TEntity : class
         {
-            return new RepositoryBase<TEntity>(_context);
+            return new RepositoryBase<TEntity, ComputerStoreDbContext>(_context);
         }
 
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+        public void CommitTransaction(out string errorMessage)
+        {
+            try
+            {
+                _transaction?.Commit();
+                errorMessage = "";
+            }
+            catch
+            {
+                RollBackAsync();
+                errorMessage = "خطا در اتصال به سرور، لطفا بعدا دوباره امتحان کنید.";
+            }
+        }
+        public async Task RollBackAsync()
+        {
+            await _transaction?.RollbackAsync();
+            _transaction = null;
+        }
         public async Task<bool> Save()
         {
             return Convert.ToBoolean(await _context.SaveChangesAsync());
         }
-
         public ValueTask DisposeAsync()
         {
+            _transaction?.DisposeAsync();
             return _context.DisposeAsync();
         }
     }
