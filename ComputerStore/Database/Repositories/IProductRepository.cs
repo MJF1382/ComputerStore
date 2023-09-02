988ae088133b5fa7147e965d9a19da5b51b5dc5e
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 11:49:10 -0700

    Adding "ProductRepository" and its interface.

---
+++ ComputerStore/Database/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using ComputerStore.Database.Entities;
+
+namespace ComputerStore.Database.Repositories
+{
+    public interface IProductRepository : IRepositoryBase<Product>
+    {
+
+    }
+}
---
+++ ComputerStore/Database/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ComputerStore.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStore.Database.Repositories
+{
+    public class ProductRepository : RepositoryBase<Product, ComputerStoreDbContext>, IProductRepository
+    {
+        public ProductRepository(ComputerStoreDbContext context) : base(context)
+        {
+
+        }
+
+        //public async Task<Product> BestSellingProducts()
+        //{
+        //    _context.ProductPurchases.
+        //}
+    }
+}
