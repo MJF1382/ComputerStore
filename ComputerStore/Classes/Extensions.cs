6e79510d0db0cb430d55975720eab86e6f090c13
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 07:30:01 -0700

    Adding "HasValue" extension for string.

---
+++ ComputerStore/Classes/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ComputerStore.Classes
+{
+    public static class Extensions
+    {
+        public static bool HasValue(this string input)
+        {
+            return string.IsNullOrEmpty(input) == false && string.IsNullOrWhiteSpace(input) == false;
+        }
+    }
+}
