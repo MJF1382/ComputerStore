7f789e44ac40e6560c3c39f59c05353cf2bdd5f8
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 11:47:31 -0700

    Adding transaction methods.

--- ComputerStore/Database/UnitOfWork/IUnitOfWork.cs
+++ ComputerStore/Database/UnitOfWork/IUnitOfWork.cs
@@ -5,6 +5,10 @@ namespace ComputerStore.Database.UnitOfWork
     public interface IUnitOfWork : IAsyncDisposable
     {
         IRepositoryBase<TEntity> RepositoryBase<TEntity>() where TEntity : class;
+
+        Task BeginTransactionAsync();
+        void CommitTransaction(out string errorMessage);
+        Task RollBackAsync();
         Task<bool> Save();
     }
 }
