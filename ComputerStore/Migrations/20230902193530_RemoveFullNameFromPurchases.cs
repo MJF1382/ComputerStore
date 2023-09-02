80432d4ec47583057d26224b15c269eda187943c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-02 13:08:29 -0700

    Adding "RemoveFullNameFromPurchases" migration.

---
+++ ComputerStore/Migrations/20230902193530_RemoveFullNameFromPurchases.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class RemoveFullNameFromPurchases : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Purchases");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Purchases",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
