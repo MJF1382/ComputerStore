df508ba3cf2b52cf62c12f0c9a233b60b3cb6bca
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-03 11:26:39 -0700

    Adding "AddingQuantityPropertyToProductPurchases" migration.

--- ComputerStore/Migrations/20230903182432_AddingQuantityPropertyToProductPurchases.Designer.cs
+++ ComputerStore/Migrations/20230903182432_AddingQuantityPropertyToProductPurchases.Designer.cs
@@ -4,6 +4,7 @@ using ComputerStore.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ComputerStore.Migrations
 {
     [DbContext(typeof(ComputerStoreDbContext))]
-    partial class ComputerStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230903182432_AddingQuantityPropertyToProductPurchases")]
+    partial class AddingQuantityPropertyToProductPurchases
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
