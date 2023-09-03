3353fac0ffd210e191cab24ece9ba645040a006c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-03 05:58:52 -0700

    Adding "GetAllPurchasesAsync" method.

---
+++ ComputerStore/Database/Repositories/IPurchaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
+
+namespace ComputerStore.Database.Repositories
+{
+    public interface IPurchaseRepository : IRepositoryBase<Purchase>
+    {
+        Task<List<PurchaseModel>> GetAllPurchasesAsync();
+    }
+}
---
+++ ComputerStore/Database/Repositories/PurchaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStore.Database.Repositories
+{
+    public class PurchaseRepository : RepositoryBase<Purchase, ComputerStoreDbContext>, IPurchaseRepository
+    {
+        public PurchaseRepository(ComputerStoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<PurchaseModel>> GetAllPurchasesAsync()
+        {
+            List<PurchaseModel> purchaseModels = new List<PurchaseModel>();
+            List<Purchase> purchases = await _context.Purchases.Include(p => p.ProductPurchases).ThenInclude(p => p.Product).ToListAsync();
+
+            foreach (Purchase purchase in purchases)
+            {
+                PurchaseModel purchaseModel = purchase;
+                purchaseModel.ProductIds = purchase.ProductPurchases.Select(p => p.Product.Id).ToList();
+
+                purchaseModels.Add(purchaseModel);
+            }
+
+            return purchaseModels;
+        }
+    }
+}
