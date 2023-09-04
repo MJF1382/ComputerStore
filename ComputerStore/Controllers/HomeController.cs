51ec05ca9fe5d750053b0ec68bed36f194fa5a44
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 09:58:55 -0700

    Adding home controller and "GetBestSellingProducts" api.

---
+++ ComputerStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ComputerStore.Classes;
+using ComputerStore.Database.Entities;
+using ComputerStore.Database.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("best-selling-products")]
+        public async Task<ApiResult> GetBestSellingProducts()
+        {
+            return new ApiResult(Status.Ok, await _unitOfWork.ProductRepository.GetBestSellingProductsAsync(1));
+        }
+    }
+}
