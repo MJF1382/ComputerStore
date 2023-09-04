ffda46947b0845d3f9beb98c24fdb72b1097d53b
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 00:14:01 -0700

    Adding "ProductPurchaseModel" class.

---
+++ ComputerStore/Models/ProductPurchaseModel.cs
@@ -0,0 +1,35 @@
+﻿using ComputerStore.Database.Entities;
+using System.Text.Json.Serialization;
+
+namespace ComputerStore.Models
+{
+    public class ProductPurchaseModel
+    {
+        public Guid ProductId { get; set; }
+
+        [JsonIgnore]
+        public Guid PurchaseId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public static implicit operator ProductPurchaseModel(ProductPurchase productPurchase)
+        {
+            return new ProductPurchaseModel()
+            {
+                ProductId = productPurchase.ProductId,
+                PurchaseId = productPurchase.PurchaseId,
+                Quantity = productPurchase.Quantity
+            };
+        }
+
+        public static implicit operator ProductPurchase(ProductPurchaseModel productPurchaseModel)
+        {
+            return new ProductPurchase()
+            {
+                ProductId = productPurchaseModel.ProductId,
+                PurchaseId = productPurchaseModel.PurchaseId,
+                Quantity = productPurchaseModel.Quantity
+            };
+        }
+    }
+}
