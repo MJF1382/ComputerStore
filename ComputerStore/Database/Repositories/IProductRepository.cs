f0bcf64266af48ef7f67a904ed22f1cf6f2b3aa0
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 09:58:23 -0700

    Adding "GetBestSellingProductsAsync" method in product repository.

--- ComputerStore/Database/Repositories/IProductRepository.cs
+++ ComputerStore/Database/Repositories/IProductRepository.cs
@@ -1,9 +1,10 @@
 ﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
 
 namespace ComputerStore.Database.Repositories
 {
     public interface IProductRepository : IRepositoryBase<Product>
     {
-
+        Task<List<ProductModel>> GetBestSellingProductsAsync(int? productCount = null);
     }
 }
--- ComputerStore/Database/Repositories/ProductRepository.cs
+++ ComputerStore/Database/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ComputerStore.Database.Entities;
+using ComputerStore.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ComputerStore.Database.Repositories
@@ -10,9 +11,31 @@ namespace ComputerStore.Database.Repositories
 
         }
 
-        //public async Task<Product> BestSellingProducts()
-        //{
-        //    _context.ProductPurchases.
-        //}
+        public async Task<List<ProductModel>> GetBestSellingProductsAsync(int? productCount = null)
+        {
+            var bestSellingProductPurchases = await (from product in _context.Products
+                                                     join productPurchase in _context.ProductPurchases on product.Id equals productPurchase.ProductId
+                                                     group productPurchase by productPurchase.ProductId into groupedProductPurchases
+                                                     orderby groupedProductPurchases.Sum(p => p.Quantity) descending
+                                                     select new
+                                                     {
+                                                         Id = groupedProductPurchases.Key,
+                                                         Quantity = groupedProductPurchases.Sum(p => p.Quantity),
+                                                         Products = groupedProductPurchases.AsQueryable().Include(p => p.Product).ToList()
+                                                     }).Select(p => p.Products).ToListAsync();
+
+            List<ProductModel> bestSellingProduct = new List<ProductModel>();
+
+            if (productCount.HasValue)
+            {
+                bestSellingProduct = bestSellingProductPurchases.SelectMany(productPurchases => productPurchases).Select(productPurchase => productPurchase.Product).DistinctBy(p => p.Id).ToList().Select<Product, ProductModel>(product => product).Take(productCount.Value).ToList();
+            }
+            else
+            {
+                bestSellingProduct = bestSellingProductPurchases.SelectMany(productPurchases => productPurchases).Select(productPurchase => productPurchase.Product).DistinctBy(p => p.Id).ToList().Select<Product, ProductModel>(product => product).ToList();
+            }
+
+            return bestSellingProduct;
+        }
     }
 }
