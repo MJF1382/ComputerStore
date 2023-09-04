d0db69347ddcb7ac873037489020b73246f88c5c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 11:12:14 -0700

    Adding "ChangingScoreDataTypeFromIntToFloatInCommentAndSatisfaction" migration.

---
+++ ComputerStore/Migrations/20230904180941_ChangingScoreDataTypeFromIntToFloatInCommentAndSatisfaction.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class ChangingScoreDataTypeFromIntToFloatInCommentAndSatisfaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Score",
+                table: "Satisfactions",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Score",
+                table: "Comments",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "Satisfactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
