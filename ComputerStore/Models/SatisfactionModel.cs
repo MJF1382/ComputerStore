7bcec64d81ab2a3d791ffc3df87c63b30fe24b83
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 11:04:34 -0700

    Adding "SatisfactionModel" class.

---
+++ ComputerStore/Models/SatisfactionModel.cs
@@ -0,0 +1,46 @@
+﻿using ComputerStore.Database.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputerStore.Models
+{
+    public class SatisfactionModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "ارسال کننده را وارد کنید.")]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "امتیاز خود را وارد کنید.")]
+        public int Score { get; set; }
+
+        [Required(ErrorMessage = "متن نظر خود را وارد کنید.")]
+        [StringLength(300, ErrorMessage = "متن نظر شما باید حداکثر 300 کاراکتر باشد.")]
+        public string Body { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public static implicit operator SatisfactionModel(Satisfaction satisfaction)
+        {
+            return new SatisfactionModel()
+            {
+                Id = satisfaction.Id,
+                UserId = satisfaction.UserId,
+                Score = satisfaction.Score,
+                Body = satisfaction.Body,
+                PublishDate = satisfaction.PublishDate
+            };
+        }
+
+        public static implicit operator Satisfaction(SatisfactionModel satisfactionModel)
+        {
+            return new Satisfaction()
+            {
+                Id = satisfactionModel.Id,
+                UserId = satisfactionModel.UserId,
+                Score = satisfactionModel.Score,
+                Body = satisfactionModel.Body,
+                PublishDate = satisfactionModel.PublishDate
+            };
+        }
+    }
+}
