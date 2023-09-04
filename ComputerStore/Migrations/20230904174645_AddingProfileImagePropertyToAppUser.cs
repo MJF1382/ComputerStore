c8a376c9d7a49ec74d57b87bf21c23b1cd575e68
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-04 11:03:27 -0700

    Adding "AddingProfileImagePropertyToAppUser" migration.

---
+++ ComputerStore/Migrations/20230904174645_AddingProfileImagePropertyToAppUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class AddingProfileImagePropertyToAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "AppUsers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "AppUsers");
+        }
+    }
+}
