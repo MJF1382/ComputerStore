196f14359ea42c4843cf705f216357fc0f8b8481
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-05 01:39:48 -0700

    Adding "GetLatestArticles" api.

--- ComputerStore/Controllers/HomeController.cs
+++ ComputerStore/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using ComputerStore.Classes;
+using ComputerStore.Database;
 using ComputerStore.Database.Entities;
 using ComputerStore.Database.Repositories;
 using ComputerStore.Database.UnitOfWork;
+using ComputerStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ComputerStore.Controllers
 {
@@ -13,11 +17,13 @@ namespace ComputerStore.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IRepositoryBase<Satisfaction> _satisfactionRepository;
+        private readonly IRepositoryBase<Article> _articleRepository;
 
         public HomeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             _satisfactionRepository = _unitOfWork.RepositoryBase<Satisfaction>();
+            _articleRepository = _unitOfWork.RepositoryBase<Article>();
         }
 
         [HttpGet("best-selling-products")]
@@ -47,6 +53,20 @@ namespace ComputerStore.Controllers
                 SuccessPurchase = successPurchase,
                 UsersCount = usersCount
             });
+        }
+
+        [HttpGet("latest-articles")]
+        public async Task<ApiResult> GetLatestArticles()
+        {
+            List<ArticleModel> articles = (await _articleRepository.FindByConditionAsync(
+                null,
+                new Expression<Func<Article, object>>[] { p => p.Category, p => p.User, p => p.Comments },
+                p => p.PublishDateTime,
+                false,
+                0,
+                3)).Select<Article, ArticleModel>(article => article).ToList();
+
+            return new ApiResult(Status.Ok, articles);
         }
     }
 }
