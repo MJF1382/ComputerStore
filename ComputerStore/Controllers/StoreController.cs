64911bf182e0df7f402651554eeca2869a43f909
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-05 05:18:11 -0700

    Adding "StoreController".

---
+++ ComputerStore/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using ComputerStore.Classes;
+using ComputerStore.Database.Entities;
+using ComputerStore.Database.UnitOfWork;
+using ComputerStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStore.Controllers
+{
+    [Route("api/store")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StoreController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
