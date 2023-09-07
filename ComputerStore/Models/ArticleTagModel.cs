c33fbaf66af2fb158a11089885bba7abcd035bc4
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-07 12:57:26 -0700

    Adding "ArticleTagModel" class.

---
+++ ComputerStore/Models/ArticleTagModel.cs
@@ -0,0 +1,28 @@
+﻿using ComputerStore.Database.Entities;
+
+namespace ComputerStore.Models
+{
+    public class ArticleTagModel
+    {
+        public Guid ArticleId { get; set; }
+        public Guid TagId { get; set; }
+
+        public static implicit operator ArticleTagModel(ArticleTag articleTag)
+        {
+            return new ArticleTagModel()
+            {
+                ArticleId = articleTag.ArticleId,
+                TagId = articleTag.TagId
+            };
+        }
+
+        public static implicit operator ArticleTag(ArticleTagModel articleTagModel)
+        {
+            return new ArticleTag()
+            {
+                ArticleId = articleTagModel.ArticleId,
+                TagId = articleTagModel.TagId
+            };
+        }
+    }
+}
