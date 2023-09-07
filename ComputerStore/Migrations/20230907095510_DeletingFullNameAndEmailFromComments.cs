043340882eee9be1110733df061d29dbc49abf09
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-07 02:56:42 -0700

    Adding "DeletingFullNameAndEmailFromComments" migration.

---
+++ ComputerStore/Migrations/20230907095510_DeletingFullNameAndEmailFromComments.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerStore.Migrations
+{
+    public partial class DeletingFullNameAndEmailFromComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Comments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Comments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Comments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
