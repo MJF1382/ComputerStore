771ed1c6669f9c900f8912e948554deb807f7efd
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-09-08 06:10:38 -0700

    Adding navigation properties to "ProductPurchaseModel".

--- ComputerStore/Models/ProductPurchaseModel.cs
+++ ComputerStore/Models/ProductPurchaseModel.cs
@@ -12,24 +12,49 @@ namespace ComputerStore.Models
 
         public int Quantity { get; set; }
 
+        public ProductModel? Product { get; set; }
+        public PurchaseModel? Purchase { get; set; }
+
         public static implicit operator ProductPurchaseModel(ProductPurchase productPurchase)
         {
-            return new ProductPurchaseModel()
+            ProductPurchaseModel productPurchaseModel = new ProductPurchaseModel()
             {
                 ProductId = productPurchase.ProductId,
                 PurchaseId = productPurchase.PurchaseId,
                 Quantity = productPurchase.Quantity
             };
+
+            if (productPurchase.Product != null)
+            {
+                productPurchaseModel.Product = productPurchase.Product;
+            }
+            if (productPurchase.Purchase != null)
+            {
+                productPurchaseModel.Purchase = productPurchase.Purchase;
+            }
+
+            return productPurchaseModel;
         }
 
         public static implicit operator ProductPurchase(ProductPurchaseModel productPurchaseModel)
         {
-            return new ProductPurchase()
+            ProductPurchase productPurchase = new ProductPurchase()
             {
                 ProductId = productPurchaseModel.ProductId,
                 PurchaseId = productPurchaseModel.PurchaseId,
                 Quantity = productPurchaseModel.Quantity
             };
+
+            if (productPurchaseModel.Product != null)
+            {
+                productPurchase.Product = productPurchaseModel.Product;
+            }
+            if (productPurchaseModel.Purchase != null)
+            {
+                productPurchase.Purchase = productPurchaseModel.Purchase;
+            }
+
+            return productPurchase;
         }
     }
 }
